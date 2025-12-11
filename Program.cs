bb056c2de849a9a8e3d158148be923146b6b618b
Acsilaa <galeengames@gmail.com>
2025-12-11 14:41:42 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -89,7 +89,13 @@ namespace Polymorphism
 
         static void Main(string[] args)
         {
-
+            int[] arr = { 1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,30};
+            UList<int> list = new UList<int>();
+            for(int i =0; i < arr.Length; i++)
+            {
+                list.Add(arr[i]);
+            }
+            Console.WriteLine(list.Get(5));
         }
     }
 }
